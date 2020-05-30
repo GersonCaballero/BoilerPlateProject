f6e82c9e34f54b46eddd57e4c9cc17581218a882
GersonCaballero <51335862+GersonCaballero@users.noreply.github.com>
2020-05-29 23:21:14 -0600

    Create new model

--- Grintsys.DomainDrivenArchtectureSample-master/aspnet-core/src/Grintsys.SROverview.EntityFrameworkCore/EntityFrameworkCore/SROverviewDbContext.cs
+++ Grintsys.DomainDrivenArchtectureSample-master/aspnet-core/src/Grintsys.SROverview.EntityFrameworkCore/EntityFrameworkCore/SROverviewDbContext.cs
@@ -3,13 +3,16 @@ using Abp.Zero.EntityFrameworkCore;
 using Grintsys.SROverview.Authorization.Roles;
 using Grintsys.SROverview.Authorization.Users;
 using Grintsys.SROverview.MultiTenancy;
+using Microsoft.EntityFrameworkCore.Internal;
+using Grintsys.SROverview.Employees;
 
 namespace Grintsys.SROverview.EntityFrameworkCore
 {
     public class SROverviewDbContext : AbpZeroDbContext<Tenant, Role, User, SROverviewDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<Employee> Employees { get; set; }
+
         public SROverviewDbContext(DbContextOptions<SROverviewDbContext> options)
             : base(options)
         {
